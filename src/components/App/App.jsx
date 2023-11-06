e3e45528134f6cdc4a9f8da4492a7c8ac2ea82c6
9Alla9 <konfetcka@gmail.com>
2023-11-06 10:57:34 +0200

    import

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,20 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Catalog from './pages/Catalog';
-import Favorites from './pages/Favorites';
-import Header from './Header/Header';
+import Home from 'pages/Home';
+import Catalog from 'pages/Catalog';
+import Favorites from 'pages/Favorites';
 import { Toaster } from 'react-hot-toast';
 import { ContainerStyled } from './App.styled';
 
 function App() {
   return (
     <ContainerStyled>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-      <Toaster position="top-right" />
+      <Toaster position="rent-cars" />
     </ContainerStyled>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { persistor, store } from './redux/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import App from './components/App';
+import App from './components/App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
       <BrowserRouter basename="/rent-cars">
         <App />
       </BrowserRouter>
-    </PersistGate>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 );
